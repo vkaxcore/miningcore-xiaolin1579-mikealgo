59565f4fcf548829a1eec3d916cedf84f049e616
Decho Kocharin <sourcedecho@outlook.com>
2023-08-05 10:22:47 +0700

    Add EvrProgpow Algorithm

--- src/Miningcore/Blockchain/Progpow/ProgpowUtils.cs
+++ src/Miningcore/Blockchain/Progpow/ProgpowUtils.cs
@@ -60,4 +60,33 @@ public static class ProgpowUtils
 
         return string.Format("{0:x64}", NewTarget);
     }
+
+    public static string EvrmoreEncodeTarget(double difficulty)
+    {
+        difficulty = 1.0 / difficulty;
+
+        BigInteger NewTarget;
+        BigInteger DecimalDiff;
+        BigInteger DecimalTarget;
+
+        NewTarget = BigInteger.Multiply(EvrmoreConstants.Diff1B, new BigInteger(difficulty));
+
+        string StringDiff = difficulty.ToString(CultureInfo.InvariantCulture);
+        int DecimalOffset = StringDiff.IndexOf(".");
+        if(DecimalOffset > -1)
+        {
+            int Precision = (StringDiff.Length - 1) - DecimalOffset;
+            DecimalDiff = BigInteger.Parse(StringDiff.Substring(DecimalOffset + 1));
+            DecimalTarget = BigInteger.Multiply(EvrmoreConstants.Diff1B, DecimalDiff);
+
+            string s = DecimalTarget.ToString();
+            s = s.Substring(0, s.Length - Precision);
+
+            DecimalTarget = BigInteger.Parse(s);
+            NewTarget += DecimalTarget;
+        }
+
+        return string.Format("{0:x64}", NewTarget);
+    }
+
 }
--- src/Miningcore/build-libs-linux.sh
+++ src/Miningcore/build-libs-linux.sh
@@ -30,6 +30,7 @@ export HAVE_FEATURE="$HAVE_AES $HAVE_SSE2 $HAVE_SSE3 $HAVE_SSSE3 $HAVE_AVX $HAVE
 (cd ../Native/libcryptonight && make -j clean && make -j) && mv ../Native/libcryptonight/libcryptonight.so "$OutDir"
 (cd ../Native/libkawpow && make -j clean && make -j) && mv ../Native/libkawpow/libkawpow.so "$OutDir"
 (cd ../Native/libfiropow && make -j clean && make -j) && mv ../Native/libfiropow/libfiropow.so "$OutDir"
+(cd ../Native/libevrprogpow && make -j clean && make -j) && mv ../Native/libevrprogpow/libevrprogpow.so "$OutDir"
 
 
 ((cd /tmp && rm -rf RandomX && git clone https://github.com/tevador/RandomX && cd RandomX && git checkout tags/v1.1.10 && mkdir build && cd build && cmake -DARCH=native .. && make -j) && (cd ../Native/librandomx && cp /tmp/RandomX/build/librandomx.a . && make -j clean && make -j) && mv ../Native/librandomx/librandomx.so "$OutDir")

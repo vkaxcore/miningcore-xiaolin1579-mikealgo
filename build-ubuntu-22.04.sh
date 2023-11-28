4287be9c344f9a0c7a1c6b2c02e3f30643a7dd41
Decho Kocharin <sourcedecho@outlook.com>
2023-11-28 22:49:25 +0700

    Update build-ubuntu-22.04.sh

--- build-ubuntu-22.04.sh
+++ build-ubuntu-22.04.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# dotnet 6 or higher is included in Ubuntu 22.04 and up
+# install install-dependencies
+sudo apt-get update; \
+  sudo apt-get -y install wget
+
+# add dotnet repo
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
 # install dev-dependencies
 sudo apt-get update; \

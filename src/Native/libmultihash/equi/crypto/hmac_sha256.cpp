f990bad9c4ccfbaf990e93b079eebf86a4408875
Decho Kocharin <sourcedecho@outlook.com>
2024-01-15 20:34:30 +0700

    Update hmac_sha256.cpp

--- src/Native/libmultihash/equi/crypto/hmac_sha256.cpp
+++ src/Native/libmultihash/equi/crypto/hmac_sha256.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2014 The Bitcoin Core developers
+// Copyright (c) 2019-2023 The Zcash developers
 // Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// file COPYING or https://www.opensource.org/licenses/mit-license.php .
 
 #include "hmac_sha256.h"
 
